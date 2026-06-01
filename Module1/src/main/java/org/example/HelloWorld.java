2ff9a739686cb4529a5a1d4ca2d3211cedab6062
Alexey2410 <alexey2410@gmail.com>
2026-06-01 23:03:45 +0300

    initial

---
+++ Module1/src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class HelloWorld {
+
+    static void main(String[] args) {
+        System.out.println ("Hello, World!");
+    }
+
+    public void someMethod() {
+        //Method code
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
